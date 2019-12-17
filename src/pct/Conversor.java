8e9a02eb485a65f66c3ad59eb2f036d949797d12
gilberto <gilberto@gilberto-C14CU51>
2019-12-17 03:13:30 -0200

    Terceiro commit

--- src/pct/Conversor.java
+++ src/pct/Conversor.java
@@ -18,19 +18,54 @@ public class Conversor {
         
         //Objeto de entrada
         Scanner entrada = new Scanner(System.in);
-        
+                            
         //Apresentação
         System.out.println("\n\t\t\t -- Conversor --\n");
         
-        //Entrada        
-        System.out.print("Informe a temperatura em ºC: ");
-        cel = entrada.nextDouble();
+        //Menu
+        System.out.println("[1] ºC --> ºF");
+        System.out.println("[2] ºF --> ºC");
+        System.out.println("[3] Sair");
 
-        //Processamento
-        fah = 9 * cel / 5 + 32;
+        System.out.print("Opção: ");
+        int op = entrada.nextInt();
         
-        //Saída
-        System.out.printf("\n\t%.2fºC = %.2fºF\n", cel, fah);
+        switch(op){        
+            case 1:
+                System.out.println("\n\t\t\t -- Cel --> Fah -- \n");
+                
+                //Entrada        
+                System.out.print("Informe a temperatura em ºC: ");
+                cel = entrada.nextDouble();
+
+                //Processamento
+                fah = 9 * cel / 5 + 32;
+        
+                //Saída
+                System.out.printf("\n\t%.2fºC = %.2fºF\n", cel, fah);
+        
+                break;
+            case 2:
+                System.out.println("\n\t\t\t -- Fah --> Cel -- \n");
+                
+                //Entrada        
+                System.out.print("Informe a temperatura em ºF: ");
+                fah = entrada.nextDouble();
+
+                //Processamento
+                cel = (fah - 32) * 5 / 9;
+        
+                //Saída
+                System.out.printf("\n\t%.2fºF = %.2fºC\n", fah, cel);
+        
+                break;
+            case 3:
+                System.out.println("Forte abraço!");
+                break;
+            default:
+                System.out.println("Opção incorreta!");
+                
+        }
                 
     }
     
